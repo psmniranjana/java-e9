e7e99974aaac15f92b78c719060cd301e1b4df20
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-04-21 15:12:34 +0530

    Add files via upload

---
+++ Double2.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgdouble;
+
+/**
+ *
+ * @author Admin
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+    
+           
+       class A {
+
+   public void doSomething() {
+
+     System.out.println("A");
+
+   }
+
+   public void doSomething(String str) {
+
+     System.out.println(str);
+
+   }
+
+}
+
+public class Double   {
+
+   public static void main(String[ ] args) {
+
+   A object = new A();
+
+   object.doSomething("B");
+
+   }
+
+}
+
+
+
+    
+  
+
